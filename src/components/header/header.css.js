95d0d77eb1c38c4ca7b636340dbfddc1c9e5bc92
Tina Roh <tinaroh@gmail.com>
2019-04-03 14:37:48 -0700

    Simple hamburger menu implementation

--- src/components/header/header.css.js
+++ src/components/header/header.css.js
@@ -1,27 +1,99 @@
 import styled from 'styled-components';
+import MEDIA from 'helpers/mediaTemplates';
 
-export const Container = styled.header`
-  background-color: #3372BC;
-  display: flex;
-  justify-content: space-between;
+export const Container = styled.div`
+  background-color: #3372bc;
+  overflow: hidden;
+  /* display: block; */
+  /* justify-content: space-between; */
   align-items: center;
-  padding: 1rem;
+  /* padding: 1rem; */
 
   a {
-    color: #FFFFFF;
+    color: #ffffff;
     transition: color 0.2s ease;
     text-decoration: none;
+    float: left;
+    display: block;
+    text-align: center;
+    padding: 10px;
+
+    &.menu {
+      height: 25px;
+      color: #ffffff;
+      font-family: Futura;
+      font-size: 18px;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
 
     &:hover {
       color: inherit;
     }
+
+    ${MEDIA.IPAD`
+    /* padding: 10px; */
+
+
+      &:not(:first-child) {
+        display: none;
+      }
+    `}
   }
-  
+
+  &.opened {
+    position: relative;
+
+    a {
+      float: none;
+      display: block;
+      text-align: left;
+    }
+  }
+
   h1 {
-    height: 42px;	color: #FFFFFF;	font-family: Futura;	font-size: 32px;	font-weight: bold;	letter-spacing: -1.28px;	line-height: 42px;	text-align: center;
+    height: 42px;
+    color: #ffffff;
+    font-family: Futura;
+    font-size: 32px;
+    font-weight: bold;
+    letter-spacing: -1.28px;
+    line-height: 42px;
 
     &:hover {
       color: inherit;
     }
   }
+
+  .fullTitle {
+    ${MEDIA.IPAD`
+      display: none;
+    `}
+  }
+
+  .shortTitle {
+    display: none;
+
+    ${MEDIA.IPAD`
+      display: block;
+    `}
+  }
+`;
+
+export const MenuButton = styled.span`
+  display: none;
+
+  ${MEDIA.IPAD`
+    display: block;
+    float: right;
+    text-align: left;
+    /* display: inline-block; */
+    padding: 24px 16px;
+
+    .opened & {
+      position: absolute;
+      right: 0;
+      top: 0;
+    }
+  `}
 `;
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,9 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import posed from 'react-pose';
-import { Container } from './header.css';
+import { Container, d, MenuButton } from './header.css';
 import Title from 'components/title';
-import Nav from 'components/header/nav';
+// import Nav from 'components/header/nav';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 // Example of a component-specific page transition
 const AnimatedContainer = posed.div({
@@ -22,20 +24,52 @@ const AnimatedContainer = posed.div({
   },
 });
 
-const Header = ({ title }) => (
-  <AnimatedContainer>
-    <Container>
-      <Link to="/">
-        <Title as="h1">{title}</Title>
-      </Link>
+class Header extends React.Component {
+  state = {
+    isOpen: false,
+  };
 
-      <Nav />
-    </Container>
-  </AnimatedContainer>
-);
+  toggleMenu = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
+
+  render() {
+    return (
+      // <AnimatedContainer>
+      <Container className={this.state.isOpen ? 'opened' : undefined}>
+        <Link to="/">
+          <Title as="h1" className="fullTitle">
+            {this.props.title}
+          </Title>
+          <Title as="h1" className="shortTitle">
+            {this.props.shortTitle}
+          </Title>
+        </Link>
+        <a className="menu" href="https://yang2020.com">
+          YANG2020
+        </a>
+        <Link className="menu" to="/submit">
+          Submit
+        </Link>
+        <a
+          className="menu"
+          href="https://secure.actblue.com/donate/friends-of-andrew-yang"
+        >
+          Donate
+        </a>
+        <MenuButton onClick={this.toggleMenu}>
+          <FontAwesomeIcon icon={faBars} color="white" size="lg" />
+        </MenuButton>
+        {/* <Nav title={title} shortTitle={shortTitle} /> */}
+      </Container>
+      // </AnimatedContainer>
+    );
+  }
+}
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  shortTitle: PropTypes.string.isRequired,
 };
 
 export default Header;
